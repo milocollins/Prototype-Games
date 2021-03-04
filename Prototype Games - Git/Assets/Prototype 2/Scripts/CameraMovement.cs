bfc536a0284860c89d8ab76ed28b3accbf1f01cb
Axion816 <m.collins1@lancaster.ac.uk>
2021-03-04 16:22:32 +0000

    File Fix

---
+++ Prototype Games - Git/Assets/Prototype 2/Scripts/CameraMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform P_1;
+    public Transform P_2;
+    public Camera camera_1;
+    public Camera camera_2;
+
+    private Camera mainCamera;   
+    private Vector3 average;
+    private bool P1InView = true;
+    private bool P2InView = true;
+    private BoxCollider2D boundary;
+    void Start()
+    {
+        boundary = GetComponent<BoxCollider2D>();
+        mainCamera = GetComponent<Camera>();
+        camera_1.enabled = false;
+        camera_2.enabled = false;
+    }
+
+    void Update()
+    {
+        
+        transform.position = Midpoint();
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.name == "Player_1")
+        {
+            P1InView = false;
+        }
+        else if (other.name == "Player_2")
+        {
+            P2InView = false;
+        }
+        camera_1.enabled = true;
+        camera_2.enabled = true;
+        mainCamera.enabled = false;
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player_1")
+        {
+            P1InView = true;
+        }
+        else if (collision.name == "Player_2")
+        {
+            P2InView = true;
+        }
+        if (P1InView && P2InView)
+        {
+            camera_1.enabled = false;
+            camera_2.enabled = false;
+            mainCamera.enabled = true;
+        }
+    }
+    private Vector3 Midpoint()
+    {
+        average = new Vector3((P_1.position.x + P_2.position.x) / 2, (P_1.position.y + P_2.position.y) / 2, transform.position.z);
+        return average;
+    }
+}
---
+++ Prototype Games - Git/Assets/Prototype 2/Scripts/PlayerScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public PlayerID ID;
+    public float moveSpeed;
+    private string IDString;
+    private Animator MyAnim;
+    private Rigidbody2D MyRigid;
+    internal float x = 0;
+    internal float y = 0;
+
+    public enum PlayerID
+    {
+        _1,
+        _2
+    }
+    void Start()
+    {
+        MyAnim = GetComponent<Animator>();
+        MyRigid = GetComponent<Rigidbody2D>();
+        switch (ID)
+        {
+            case PlayerID._1:
+                IDString = "_1";
+                break;
+            case PlayerID._2:
+                IDString = "_2";
+                break;
+            default:
+                break;
+        }
+    }
+
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal" + IDString);
+        y = Input.GetAxis("Vertical" + IDString);
+        /*if (x > y)
+        {
+            y = 0;
+        }
+        else if (y > x)
+        {
+            x = 0;
+        }*/
+        MyRigid.velocity = new Vector2(x * moveSpeed, y * moveSpeed);
+        MoveAnimation();
+    }
+    private void MoveAnimation()
+    {
+        MyAnim.SetFloat(Animator.StringToHash("vY"), y);
+        MyAnim.SetFloat(Animator.StringToHash("vX"), x);
+        if (x == 0 && y == 0)
+        {
+            MyAnim.SetBool(Animator.StringToHash("isIdle"), true);
+        }
+        else
+        {
+            MyAnim.SetBool(Animator.StringToHash("isIdle"), false);
+        }
+    }
+}
